be90c6091b1c41a88d43d085793116afa611c205
ar.a.belyaev <ar.a.belyaev@tinkoff.ru>
2024-10-31 18:03:19 +0300

    отправка фото на команду start

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,11 +1,39 @@
-import {Injectable} from "@nestjs/common";
+import {Injectable, OnModuleInit} from "@nestjs/common";
 const TelegramBot = require('node-telegram-bot-api');
 
 import {ConfigService} from "@nestjs/config";
+import * as fs from "node:fs";
 
 @Injectable()
-export class BotService {
-  constructor(private readonly configService: ConfigService) {}
+export class BotService implements OnModuleInit {
+  public bot: any;
+  constructor(private readonly configService: ConfigService) {
+    this.bot = new TelegramBot(this.configService.get('TELEGRAM_BOT_TOKEN'), { polling: true });
+  }
 
-  bot = new TelegramBot(this.configService.get('TELEGRAM_BOT_TOKEN'), { polling: true });
+  onModuleInit() {
+    this.initializeBotHandlers();
+  }
+
+  initializeBotHandlers() {
+    this.bot.onText(/\/start/, (msg) => {
+      const chatId = msg.chat.id;
+      const caption = `🎁 Here you can buy and end gifts to your friends.`;
+      const options = {
+        caption,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Open App',
+                web_app: { url: this.configService.get('TELEGRAM_MINI_APP_DIRECT_URL') },
+              },
+            ],
+          ],
+        },
+      };
+      const photoPath = fs.readFileSync('/usr/src/app/src/assets/img.png'); // Путь к локальному изображению
+      this.bot.sendPhoto(chatId, photoPath, options);
+    });
+  }
 }
