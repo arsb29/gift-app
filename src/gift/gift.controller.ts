6f0e8ce78dac6ad40a0e463316c395931bac120f
ar.a.belyaev <ar.a.belyaev@tinkoff.ru>
2024-11-08 17:51:51 +0300

    запрос одного подарка

--- src/gift/gift.controller.ts
+++ src/gift/gift.controller.ts
@@ -1,12 +1,18 @@
-import {Body, Controller, Post} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post} from "@nestjs/common";
 import {GiftService} from "./gift.service";
 import {GiftsDto} from "./gift.dto";
+import {Types} from "mongoose";
 
 @Controller('api/store')
 export class GiftController {
   constructor(
     private giftService: GiftService
   ) {}
+
+  @Get(':id')
+  async getGift(@Param('id') id: Types.ObjectId) {
+    return this.giftService.getGiftById({_id: id});
+  }
 
   @Post()
   async getAllGifts(@Body() body: GiftsDto) {
