0dccf07d94fa320ccf52f72c81249954aff1f558
ar.a.belyaev <ar.a.belyaev@tinkoff.ru>
2024-11-10 01:40:48 +0300

    исправлено удаление истекших транзакций

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -109,7 +109,10 @@ export class TransactionService {
 
   async deleteExpiredTransactions() {
     const now = Date.now();
-    return await this.transactionModel.deleteMany({expiresIn: {$lt: now}}).exec();
+    return await this.transactionModel.deleteMany({
+      $or: [{status: TRANSACTION_STATUS.transactionCreated}, {status: TRANSACTION_STATUS.transactionCreated}],
+      expiresIn: {$lt: now}
+    }).exec();
   }
 
   async getGroupedBookedGifts() {
